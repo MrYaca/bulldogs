3ae2f64ec037066f002b946bb6c092154e4da67e
Sergio Brito <sergiobritor@gmail.com>
2022-09-11 03:36:06 -0400

    feat(03_01): integrate storybook to repo

---
+++ libs/store/merchandise/src/lib/store-merchandise.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoreMerchandise } from './store-merchandise';
+
+export default {
+  component: StoreMerchandise,
+  title: 'StoreMerchandise',
+} as ComponentMeta<typeof StoreMerchandise>;
+
+const Template: ComponentStory<typeof StoreMerchandise> = (args) => (
+  <StoreMerchandise {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
