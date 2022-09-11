27930a8683a66a5c78accd9b34b5fdd9d84b402c
Sergio Brito <sergiobritor@gmail.com>
2022-09-11 04:18:36 -0400

    feat(03_03): write story variants

---
+++ libs/store/merchandise/src/lib/product/product.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Product } from './product';
+
+export default {
+  component: Product,
+  title: 'Store/Product',
+} as ComponentMeta<typeof Product>;
+
+const Template: ComponentStory<typeof Product> = (args) => (
+  <Product {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
--- libs/store/merchandise/src/lib/store-merchandise.stories.tsx
+++ libs/store/merchandise/src/lib/store-merchandise.stories.tsx
@@ -3,14 +3,19 @@ import { StoreMerchandise } from './store-merchandise';
 
 export default {
   component: StoreMerchandise,
-  title: 'StoreMerchandise',
+  title: 'Store/Merchandise',
 } as ComponentMeta<typeof StoreMerchandise>;
 
 const Template: ComponentStory<typeof StoreMerchandise> = (args) => (
   <StoreMerchandise {...args} />
 );
 
-export const Primary = Template.bind({});
-Primary.args = {
+export const Inactive = Template.bind({});
+Inactive.args = {
   title: 'Hola Mundo',
+};
+
+export const Active = Template.bind({});
+Active.args = {
+  title: 'Hola a todos',
 };
