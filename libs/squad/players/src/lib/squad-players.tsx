700858faa5921278072fd1147be327adb6d02bf5
Sergio Brito <sergiobritor@gmail.com>
2022-09-25 12:39:09 -0400

    feat(05_03): implement Factory

--- libs/squad/players/src/lib/squad-players.tsx
+++ libs/squad/players/src/lib/squad-players.tsx
@@ -3,6 +3,32 @@ import styles from './squad-players.module.scss';
 /* eslint-disable-next-line */
 export interface SquadPlayersProps {}
 
+interface Player {
+  name: string;
+  number: number;
+  age: number;
+  position: string;
+}
+
+const createPlayer = ({ name, number, age, position }: Player) => ({
+  name,
+  number,
+  age,
+  position,
+  description() {
+    return `${name} → ${position}`;
+  },
+});
+
+const player1 = createPlayer({
+  name: 'Sergio',
+  number: 35,
+  age: 40,
+  position: 'Fullback',
+});
+
+player1.description(); //?
+
 export function SquadPlayers(props: SquadPlayersProps) {
   return (
     <div className={styles['container']}>
