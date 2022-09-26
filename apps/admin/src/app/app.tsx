87515fc1cad93dd21d3737682ed413654eab4390
Sergio Brito <sergiobritor@gmail.com>
2022-09-26 08:45:50 -0400

    CI(06_01): add the first build ap

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,13 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { StoreMerchandise } from '@store/merchandise';
 import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
 
 export function App() {
   return (
-    <>
-      <NxWelcome title="admin" />
-      <div />
-    </>
+    <StoreMerchandise
+      title={''}
+      avatar={''}
+      name={''}
+      active={false}
+    ></StoreMerchandise>
   );
 }
 
